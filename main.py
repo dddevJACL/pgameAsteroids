2483537ee75a4311995836d8dac061b7d0c16eac
Daniel Dalinda <daniel.dalinda.dev@gmail.com>
2025-01-02 11:41:22 +0900

    add clock to main.py

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         screen.fill("black")
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
